2e884611300a03ac139aa89cd55d5fe0c0211b0a
Eymeric Chauchat <eymeric.chauchat@gmail.com>
2024-11-19 14:34:55 +0900

    Version control (basic)

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'Python xL-Sindy'
 copyright = '2024, Eymeric Chauchat'
 author = 'Eymeric Chauchat'
-release = 'V1.0.0'
+release = 'V0.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
